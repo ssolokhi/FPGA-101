8aa8d9df360a9c5b937b477ca26320d0bb5a349d
ssolokhi <solokhin02@gmail.com>
2026-01-23 16:40:53 +0100

    remove dump to debug file

--- and_gate/and_gate_tb.v
+++ and_gate/and_gate_tb.v
@@ -10,7 +10,6 @@ module and_gate_tb();
     );
     initial // start providing test inputs to the AND gate
         begin
-            $dumpfile("tb_dump.vcd"); $dumpvars; // required for simulations in EDA Playground
             r_test_input_1 <= 1'b0;
             r_test_input_2 <= 1'b0;
             #10; // wait fot 10 ns. Works only in simulation, real FPGA's don't know what time is
