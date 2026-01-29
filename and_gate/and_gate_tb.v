cd13f86e8bbafe0250fff8a9410822bf806bb978
ssolokhi <solokhin02@gmail.com>
2026-01-29 11:48:44 +0100

    Improve testbench robustness with assert statements

--- and_gate/and_gate_tb.v
+++ and_gate/and_gate_tb.v
@@ -13,15 +13,21 @@ module and_gate_tb();
             r_test_input_1 <= 1'b0;
             r_test_input_2 <= 1'b0;
             #10; // wait fot 10 ns. Works only in simulation, real FPGA's don't know what time is
+            assert (w_test_output == 1'b0);
+
             r_test_input_1 <= 1'b1;
             r_test_input_2 <= 1'b0;
             #10;
+            assert (w_test_output == 1'b0);
+
             r_test_input_1 <= 1'b0;
             r_test_input_2 <= 1'b1;
             #10;
+            assert (w_test_output == 1'b0);
+
             r_test_input_1 <= 1'b1;
             r_test_input_2 <= 1'b1;
             #10;
-            $finish();
+            assert (w_test_output == 1'b1);
         end
 endmodule
